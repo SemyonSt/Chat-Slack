70fbc39f99613bd7303a5f4721a03487276fac54
Семён <sema.stulnikov@mail.ru>
2023-04-06 17:52:37 +0300

    fix btn

--- frontend/src/pages/LoginPages.jsx
+++ frontend/src/pages/LoginPages.jsx
@@ -29,15 +29,13 @@ const Logopages = () => {
   const notify = () => toast.error('Ошибка сети');
   const { t } = useTranslation();
   const [isLoading, setIsLoading] = useState(false);
+  // console.log(isLoading);
 
   const schema = yup.object().shape({
-    username: yup.string()
-      .min(3, t('error.minMaxSymbols'))
-      .max(20, t('error.minMaxSymbols'))
-      .required(t('error.required')),
-    password: yup.string()
-      .min(5, 'Password is to short')
-      .required(t('error.required')),
+    username: yup
+      .string(),
+    password: yup
+      .string(),
   });
 
   // Добавление фокусов на инпуты
@@ -73,26 +71,30 @@ const Logopages = () => {
       setIsLoading(true);
       axios.post(routes.login(), { username: values.username, password: values.password })
         .then((response) => {
-          console.log('!!!!!!!!!121212', crypto.randomUUID());
           const data = JSON.stringify(response.data);
           localStorage.clear();
           localStorage.setItem('userInfo', data);
           navigate('/');
           setToken(response.data);
+          console.log('TRUUUEEEE');
         })
         .catch((err) => {
           if (err.message === 'Network Error') {
             return notify();
           }
           if (err.response.status === 401) {
-            errors.password = t('error.invalidNameOrPass');
+            // usernameRef.current.focus();
+            console.log('ERROOR');
+            errors.username = t('error.invalidNameOrPass');
             return setSubmitting(false);
           }
           return setSubmitting(false);
         })
         .finally(() => {
-          console.log('YES');
+          console.log('OOOOY');
+          // console.log('YES');
           setIsLoading(false); // сброс isLoading в false после завершения запроса
+          setSubmitting(true);
         });
     },
   });
@@ -115,58 +117,58 @@ const Logopages = () => {
                 />
               </div>
               <form onSubmit={handleSubmit} className="col-12 col-md-6 mt-3 mt-mb-0">
-              <Form.Group>
-                <h1 className="text-center mb-4">{t('loginPages.entrance')}</h1>
-                <div className="form-floating mb-3">
-                  <Form.Control
-                    ref={usernameRef}
-                    onKeyDown={(event) => handleKeyDown(event, passwordRef)}
-                    name="username"
-                    autoComplete="username"
-                    required=""
-                    placeholder="Ваш ник"
-                    id="username"
-                    onChange={handleChange}
-                    value={values.username}
-                    className={errClass}
-                  />
-                  <label
-                    htmlFor="username"
-                  >
-                    {t('loginPages.nickname')}
-                  </label>
-                </div>
-                <div className="form-floating mb-4">
-                  <Form.Control
-                    ref={passwordRef}
-                    onKeyDown={(event) => handleKeyDown(event, btnRef)}
-                    name="password"
-                    autoComplete="current-password"
-                    required=""
-                    placeholder={t('loginPages.password')}
-                    type="password"
-                    id="password"
-                    onChange={handleChange}
-                    value={values.password}
-                    className={errClass}
-                  />
-                  <div className="invalid-tooltip">{errors.password}</div>
-                  <label
-                    className="form-label"
-                    htmlFor="password"
-                  >
-                    {t('loginPages.password')}
-                  </label>
-                </div>
-                <Button
-                  disabled={isLoading}
-                  ref={btnRef}
-                  type="submit"
-                  className="w-100 mb-3 btn btn-primary"
-                  onClick={handleSubmit}
-                >{t('loginPages.entrance')}
-                </Button>
-              </Form.Group>
+                <Form.Group>
+                  <h1 className="text-center mb-4">{t('loginPages.entrance')}</h1>
+                  <div className="form-floating mb-3">
+                    <Form.Control
+                      ref={usernameRef}
+                      onKeyDown={(event) => handleKeyDown(event, passwordRef)}
+                      name="username"
+                      autoComplete="username"
+                      required=""
+                      placeholder="Ваш ник"
+                      id="username"
+                      onChange={handleChange}
+                      value={values.username}
+                      className={errClass}
+                    />
+                    <label
+                      htmlFor="username"
+                    >
+                      {t('loginPages.nickname')}
+                    </label>
+                  </div>
+                  <div className="form-floating mb-4">
+                    <Form.Control
+                      ref={passwordRef}
+                      onKeyDown={(event) => handleKeyDown(event, btnRef)}
+                      name="password"
+                      autoComplete="current-password"
+                      required=""
+                      placeholder={t('loginPages.password')}
+                      type="password"
+                      id="password"
+                      onChange={handleChange}
+                      value={values.password}
+                      className={errClass}
+                    />
+                    <div className="invalid-tooltip">{ errors.username}</div>
+                    <label
+                      className="form-label"
+                      htmlFor="password"
+                    >
+                      {t('loginPages.password')}
+                    </label>
+                  </div>
+                  <Button
+                    disabled={isLoading}
+                    ref={btnRef}
+                    type="submit"
+                    className="w-100 mb-3 btn btn-primary"
+                    onClick={() => usernameRef.current.focus()}
+                  >{t('loginPages.entrance')}
+                  </Button>
+                </Form.Group>
               </form>
             </div>
             <div className="card-footer p-4">
